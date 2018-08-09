99e8d79486af04891c27cb47b464cc13a8a26120
Motion Design Studio <nospam@nospam.com>
2018-08-08 23:21:51 -0400

    Experiements 1

--- regexExercises.pl
+++ regexExercises.pl
@@ -21,25 +21,47 @@ my Str $ex4 = "The first word not all that starts with alpabet letter 'a' :: ";
 
 printf "%s \n", $ex4 ~ $ex4.match(/a\w+/, :global)[0];
 
-my Str $ex5 = "Match all the words of a string starting with a lowercase vowel :: ";
+my Str $ex5 = "Match all the words of a string starting with a lowercase vowel amen :: ";
 
-#printf "%s \n", $ex5 ~ $ex5.match(/ <[aeiou]> <|w> <|w> \s /, :global);
+printf "%s \n", $ex5 ~  $ex5.match(/ <|w><[aeiou]>\w*<|w> /, :global); 
 
-#say "Match all the words of a string starting with a lowercase vowel :: ".match(/ <[aeiou]><|w><|w>\s /, :global);
+printf "%s \n", $ex5 ~  $ex5.match(/ <|w><[aeiou]>\w* /, :global); 
 
-say "Match all the words of a string starting with a lowercase vowel amen :: ".match(/ <|w><[aeiou]>\w*<|w> /, :global); 
+printf "%s \n", $ex5 ~  $ex5.match(/ << <[aeiou]>\w* >> /, :global);
 
-say "Match all the words of a string starting with a lowercase vowel amen :: ".match(/ <|w><[aeiou]>\w* /, :global); 
-
-say "Match all the words of a string starting with a lowercase vowel amen :: ".match(/ << <[aeiou]>\w* >> /, :global);
-
-say "Match all the words of a string starting with a lowercase vowel amen :: ".words.grep: *.starts-with: any <a e i o u>;
+printf "%s \n", $ex5 ~  $ex5.words.grep: *.starts-with: any <a e i o u>;
 
 # <|w> starting word boundary
 # <[aeiou]> character class a,e,i,o,u this after <|w> mean any word starting with these letters
 # \w matches a single word character, i.e. a letter
 # * after \w means zero or more characters
-# <|w> means word boundary ending with a character or not
+# <|w> means word boundary ending with a character or not it is not necessary 
+
+my $ex6 = "French mobile telephone number have 10 digits and start with “06” or “07” such as 061234567P 0712345678 not 2151234567 0787654321 :: ";
+
+printf "%s \n", $ex6 ~  $ex6.match(/ <|w>0<[67]>\d **8 /, :global);
+
+# <|w> is the word boundary stating must start with a 0 in this case
+# <[67]> is a character class and means 6 or 7 so it will match 06 or 07
+# \d means match a numberical character only
+# **8 means match 8 number characters after the 0 and 6 or 7
+
+my $ex7 = "The first word Of a string starting with a vowel in either upper- Or lowercase :: ";
+
+printf "%s \n", $ex7 ~  $ex7.match( / <|w>:i<[aeiou]>\w* /, :global)[0];
+
+# The :i outside the characte class means case insensative
+# .match creates an array of matches because of the :global it grab all it can find.  
+# [0] is the first position of the array
+
+printf "%s \n", $ex7 ~  $ex7.match( / <|w>:i<[aeiou]>\w* /);
+
+# remove the :global the matching stops at the first positive match ie Of
+
+my $ex8 = "The first occurrence of a double letter (the same letter twice in a row) bookkeeping see three morass :: ";
+
+printf "%s \n", $ex8 ~  $ex8.match( / :i\w\1+ /, :global);
+
 
 # Importants Links
 # https://docs.perl6.org/language/regexes
@@ -50,3 +72,6 @@ say "Match all the words of a string starting with a lowercase vowel amen :: ".w
 # https://www.perl.com/pub/2000/11/begperl3.html/
 # https://www.perlmonks.org/?node_id=944253
 # https://docs.perl6.org/language/regexes
+# https://perldoc.perl.org/perlrecharclass.html
+# https://docs.perl6.org/routine/match
+
